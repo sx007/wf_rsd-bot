495ffc2f25f30bb9447bebfcf9250d015cb3d978
Ivan <42902953+sx007@users.noreply.github.com>
2018-09-02 13:50:59 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,85 +1,15 @@
-const Discord = require ('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-/* Задаётся шаблон для команд */
-function commandIS(str, msg){
-    return msg.content.toLowerCase().startsWith("!" + str);
-}
 
-function pluck(array) {
-    return array.map(function(item) { return item["name"]; });
-}
-/* Роли */
-function hasRole(mem, role){
-    if (pluck(mem.roles).includes(role)){
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-/* Показывает что бот в сети */
 client.on('ready', () => {
-    console.log('Бот в сети!');
-})
-/* команды сообщений */
-client.on('message', message => {
-    var args = message.content.split(/[ ]+/);
-    /* команда привет */
-    if(commandIS("привет", message)){
-        message.reply(' привет!');
-    }
-    /* команда ютюб */
-    if(commandIS("ютюб", message)){
-        if(args.length === 1){
-            message.channel.send('Ты не указал аргумент. Использу: `!ютюб [номер эпизода]`');
-        } 
-        else if(args.length === 2){
-            message.channel.send('Привет, Ютюб. Это эпизод '+ args[1]);
-        } 
-        else{
-            message.channel.send('Ты указал много аргументов. Использу: `!ютюб [номер эпизода]`');
-        }
-    }
-    /* команда скажи */
-    if(commandIS("скажи", message)){
-        /* вот тут разделяются права */
-        if(hasRole(message.member, "Администратор") || hasRole(message.member, "Модераторы")){
-            if(args.length === 1){
-            message.channel.send('Ты не указал аргумент. Использу: `!скажи [что сказать]`');
-        } else {
-        message.channel.send(args.join(" ").substring(7));
-        }
-     } else {
-        message.channel.send('Ты не `Администратор` или `Модератор`');
-    }
-}
-    /* Удаление сообщения */
-    if(commandIS("удалить", message)) {
-        if(hasRole(message.member, "Администратор") || hasRole(message.member, "Модераторы")){
-            if(args.length >= 3){
-            message.channel.send('Ты указал много аргументов. Использу: `!удалить (количество сообщений)`');
-        } else {
-            var msg;
-            if(args.length === 1) {
-                msg=2;
-            } else {
-                msg=parseInt(args[1]) + 1;
-            }
-            message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-        }
-     } else {
-        message.channel.send('Ты не `Администратор` или `Модератор`');
-    }
-    }
-    if(commandIS("кик", message)) {
-        if(hasRole(message.member, "Администратор") || hasRole(message.member, "Модераторы")){
-            if(args.length === 1){
-            message.channel.send('Ты не указал аргумент. Использу: `!кик [кого выгнать с сервера]`');
-        } else {
-        message.guild.member(message.mentions.users.first()).kick();
-        }
-    }
-    }
+    console.log('I am ready!');
 });
 
-client.login('BOT_TOKEN');
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
