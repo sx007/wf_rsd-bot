bedfef0ebc21b8ab310fc89d6e125d6d12b3f54f
Ivan <42902953+sx007@users.noreply.github.com>
2020-01-03 22:05:33 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,11 +31,11 @@ request('http://api.warface.ru/rating/monthly?server=1&clan='+ encodeURI('Рез
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
-  var jsonContent = JSON.parse(body);
+  //var jsonContent = JSON.parse(body);
 
-  console.log(
-    jsonContent.JSON.filter(({ clan }) => clan === 'РезидентыВарфайс')
-  );
+  //console.log(
+  //  jsonContent.JSON.filter(({ clan }) => clan === 'РезидентыВарфайс')
+  //);
   
 });
 
