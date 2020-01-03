f714cf1166ff5afc6ea55d53a9ae35f23513f6a5
Ivan <42902953+sx007@users.noreply.github.com>
2020-01-03 21:03:28 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.on('ready', () => {
 
 
 const request = require('request');
-request('https://www.google.com', function (error, response, body) {
+request('https://aws.random.cat/meow', function (error, response, body) {
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
