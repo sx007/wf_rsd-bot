e399ad1677fd2072ae89467d7d6592f2f3f79937
Ivan <42902953+sx007@users.noreply.github.com>
2020-01-03 22:45:24 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,7 @@ const request = require('request');
 
 const options = {
     url: 'http://api.warface.ru/rating/monthly?server=1&clan=%D0%A0%D0%B5%D0%B7%D0%B8%D0%B4%D0%B5%D0%BD%D1%82%D1%8B%D0%92%D0%B0%D1%80%D1%84%D0%B0%D0%B9%D1%81',
+    method: 'GET',
     headers: {
         'Content-Type': 'application/json; charset=utf-8',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
