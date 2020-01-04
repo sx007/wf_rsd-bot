f6c4740dec8292f846086b350b8732761979aca1
Ivan <42902953+sx007@users.noreply.github.com>
2020-01-04 17:10:53 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ client.on('message', message => {
             const embed = new Discord.RichEmbed()
             .setTitle(":crossed_swords: Ежемесячный рейтинг клана")
             .setColor(0xFFF100)
-            .setDescription('`Название клана:`   **' + data.clan + '**\n`Глава клана:`  **' + data.clan_leader + '**\n*Бойцов в клане:*   **' + data.members + '**\n*Лига:*   **' + data.liga + '**\n*Очков за месяц:*   **' + data.points + '**\n*Место в лиге:*   **' + data.rank + '**\n*Изменение места:*   **' + data.rank_change + '**')
+            .setDescription('`Название клана:`   **' + data.clan + '**\n`Глава клана:`  **' + data.clan_leader + '**\n`Бойцов в клане:`   **' + data.members + '**\n`Лига:`   **' + data.liga + '**\n`Очков за месяц:`   **' + data.points + '**\n`Место в лиге:`   **' + data.rank + '**\n`Изменение места:`   **' + data.rank_change + '**')
             .setFooter("Бот клана", "")
             .setTimestamp()
             //.addField("Название клана: **" + data.clan + "**")
